d0e76fdc7898a67e2b8ef710c984f91a98ee0012
Chris Harris <chrisonefournine@hotmail.com>
2017-09-11 22:27:33 +0100

    Working test for file upload
    Need to look at breaking out of Init method.

--- pages/file_upload_page.py
+++ pages/file_upload_page.py
@@ -8,25 +8,20 @@ class FileUploadPage(BasePage):
     _file_submit = {"by": By.ID, "value": "file-submit"}
     _uploaded_files = {"by": By.ID, "value": "uploaded-files"}
 
-    def __init__(self, driver, filename):
+    def __init__(self, driver):
         self.driver = driver
-        self._visit("http://the-internet.herokuapp.com/upload")
-
-    def upload_file(self, filename):
-        self.filename = filename
         filename = 'some-file.rtf'
         print ('filename done')
         file = os.path.join(os.getcwd(), filename)
+        self._visit("http://the-internet.herokuapp.com/upload")
         self._type(self._file_upload, file)
         print ('done typing')
         self._click(self._file_submit)
         print ('submitted')
-        upload_file_title = self._verify_text(self._uploaded_files)
-        print ('uploaded file')
-        assert upload_file_title == "uploaded file should be %s" % filename
+        self._wait_for_is_displayed(self._uploaded_files, 10)
+        print ('uploaded displayed')
+        assert self._verify_text(self._uploaded_files) == filename,"uploaded file should be %s" % filename
         print ('text matches')
 
     def upload_success(self):
-        self._wait_for_is_displayed(self._uploaded_files, 10)
-        print ('uploaded displayed')
         return self._is_displayed(self._uploaded_files)
